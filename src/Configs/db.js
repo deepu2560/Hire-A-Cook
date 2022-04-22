230a7648b5ed3a3487e716bba08060c891b5288c
unknown <deepanshugulia256@gmail.com>
2022-04-22 19:17:12 +0530

    register and login model and controller made

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // connect scope for connecting with database
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://cookAuth:deepanshu@cluster0.2sw65.mongodb.net/deepanshu?retryWrites=true&w=majority",
+    "mongodb+srv://cookAuth:deepanshu@cluster0.2sw65.mongodb.net/chef?retryWrites=true&w=majority",
   );
 };
 
