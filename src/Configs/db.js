ffd58f66702729091910f01b29e4a092d4d512b8
unknown <deepanshugulia256@gmail.com>
2022-04-22 18:32:46 +0530

    Started working

---
+++ src/Configs/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// connect scope for connecting with database
+const connect = () => {
+  return mongoose.connect(
+    "mongodb+srv://cookAuth:deepanshu@cluster0.2sw65.mongodb.net/deepanshu?retryWrites=true&w=majority",
+  );
+};
+
+// exporting connect scople
+module.exports = connect;
