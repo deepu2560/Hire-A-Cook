9109773b59630766f460c2d3a7e83a8913de7fce
unknown <deepanshugulia256@gmail.com>
2022-04-22 20:39:29 +0530

    Auth backend is done

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const connect = require("./src/Configs/db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const userConroller = require("./src/Controllers/userController");
+
+app.use("/auth", userConroller);
+
+app.listen(8080, async () => {
+  try {
+    await connect();
+
+    console.log("Server started");
+  } catch (error) {
+    console.log("Unable to start server");
+  }
+});
--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,1 +1,24 @@
 const express = require("express");
+
+const User = require("../Models/userModel");
+
+const router = express.Router();
+
+const { register, login } = require("./authController");
+
+router.get("", async (req, res) => {
+  try {
+    const user = await User.find().lean().exec();
+
+    res.status(201).send({ error: false, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: true, token: "Server error" });
+  }
+});
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+module.exports = router;
