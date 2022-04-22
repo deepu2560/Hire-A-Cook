c60b6cf0430fd36649b3afe7ca47904c666ed97f
unknown <deepanshugulia256@gmail.com>
2022-04-23 03:11:35 +0530

    footer is done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,122 +1,128 @@
-import React from "react"
-import "../Styles/Footer.css"
+import React from "react";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MailIcon from "@mui/icons-material/Mail";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import "../Styles/Footer.css";
 
-export const Footer = ()=>{
-    return (
-        <div>
-        <footer className={"footer-section"}>
+export const Footer = () => {
+  return (
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-cta">
+          <div className="contact-detail-div">
+            <div>
+              <div className="single-cta">
+                <GitHubIcon sx={{ color: "#ff5e14", fontSize: "50px" }} />
+                <div className="cta-text">
+                  <h4>GitHub Repo</h4>
+                  <span>
+                    <a
+                      href="https://github.com/Deepu2560/Hire-A-Cook"
+                      target="_blank"
+                    >
+                      Click Me
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="single-cta">
+                <GitHubIcon sx={{ color: "#ff5e14", fontSize: "50px" }} />
+                <div className="cta-text">
+                  <h4>GitHub Backend</h4>
+                  <span>
+                    <a
+                      href="https://github.com/Deepu2560/Hire-A-Cook/tree/Backend"
+                      target="_blank"
+                    >
+                      Click Me
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="single-cta">
+                <GitHubIcon sx={{ color: "#ff5e14", fontSize: "50px" }} />
+                <div className="cta-text">
+                  <h4>GitHub Frontend</h4>
+                  <span>
+                    <a
+                      href="https://github.com/Deepu2560/Hire-A-Cook/tree/Frontend"
+                      target="_blank"
+                    >
+                      Click Me
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-content">
+          <div className="row">
+            <div className=" col-lg-4 mb-50">
+              <div className="footer-widget">
+                <div className="footer-text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
+                    sed do eiusmod tempor incididuntut consec tetur adipisicing
+                    elit,Lorem ipsum dolor sit amet.
+                  </p>
+                </div>
+                <div class="footer-social-icon">
+                  <span>Follow us</span>
+                  <a href="">
+                    <FacebookIcon sx={{ color: "#3b5998", fontSize: "50px" }} />
+                  </a>
+                  <a href="">
+                    <TwitterIcon sx={{ color: "#55acee", fontSize: "50px" }} />
+                  </a>
+                  <a href="">
+                    <GoogleIcon sx={{ color: "#dd4b39", fontSize: "50px" }} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright-area">
         <div className="container">
-            <div className="footer-cta pt-5 pb-5">
-                <div className="row">
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="fas fa-map-marker-alt"></i>
-                            <div className="cta-text">
-                                <h4>Find us</h4>
-                                <span>1010 Avenue, sw 54321, chandigarh</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="fas fa-phone"></i>
-                            <div className="cta-text">
-                                <h4>Call us</h4>
-                                <span>9876543210 0</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="far fa-envelope-open"></i>
-                            <div className="cta-text">
-                                <h4>Mail us</h4>
-                                <span>mail@info.com</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>Copyright &copy; 2020, All Right Reserved</p>
+              </div>
             </div>
-            <div className="footer-content pt-5 pb-5">
-                <div className="row">
-                    <div className="col-xl-4 col-lg-4 mb-50">
-                        <div className="footer-widget">
-                            {/* <div className="footer-logo">
-                                <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo"/></a>
-                            </div> */}
-                            <div className="footer-text">
-                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
-                                elit,Lorem ipsum dolor sit amet.</p>
-                            </div>
-                            {/* <div className="footer-social-icon">
-                                <span>Follow us</span>
-                                <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
-                                <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
-                                <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
-                            </div> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                        <div className="footer-widget">
-                            {/* <div className="footer-widget-heading">
-                                <h3>Useful Links</h3>
-                            </div> */}
-                            {/* <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">about</a></li>
-                                <li><a href="#">services</a></li>
-                                <li><a href="#">portfolio</a></li>
-                                <li><a href="#">Contact</a></li>
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Our Services</a></li>
-                                <li><a href="#">Expert Team</a></li>
-                                <li><a href="#">Contact us</a></li>
-                                <li><a href="#">Latest News</a></li>
-                            </ul> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                        <div className="footer-widget">
-                            {/* <div className="footer-widget-heading">
-                                <h3>Subscribe</h3>
-                            </div> */}
-                            {/* <div className="footer-text mb-25">
-                                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
-                            </div> */}
-                            {/* <div className="subscribe-form">
-                                <form action="#">
-                                    <input type="text" placeholder="Email Address"/>
-                                    <button><i className="fab fa-telegram-plane"></i></button>
-                                </form>
-                            </div> */}
-                        </div>
-                    </div>
-                </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li>
+                    <a href="">Home</a>
+                  </li>
+                  <li>
+                    <a href="">Terms</a>
+                  </li>
+                  <li>
+                    <a href="">Privacy</a>
+                  </li>
+                  <li>
+                    <a href="">Policy</a>
+                  </li>
+                  <li>
+                    <a href="">Contact</a>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </div>
         </div>
-        <div className="copyright-area">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-                        <div className="copyright-text">
-                            <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
-                        </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                        <div className="footer-menu">
-                            {/* <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Terms</a></li>
-                                <li><a href="#">Privacy</a></li>
-                                <li><a href="#">Policy</a></li>
-                                <li><a href="#">Contact</a></li>
-                            </ul> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </footer>
-    </div>
-    )
-}
+  );
+};
